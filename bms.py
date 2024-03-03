ce912a8b357954532e6886f27379aadefd1c0edb
asishraz <iamasish3001@gmail.com>
2024-03-03 08:57:38 +0530

    1 module of adding cust details added

--- bms.py
+++ bms.py
@@ -1,1 +1,23 @@
-print("bakery management system")
+class Bakery:
+    def __init__(self):
+        self.customers = {}
+        self.orders_counter = 1
+
+    def add_customer_details(self, name, order, quantity, date):
+        order_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        customer_id = self.orders_counter 
+        self.orders_counter += 1
+
+        self.customers[customer_id] = {
+            "name: ": name,
+            "order: ": order,
+            "date of order: ": date,
+            "quantity: ": quantity,
+
+        }
+        print("customer added successfully")
+
+
+
+
+bakery_system = Bakery()
